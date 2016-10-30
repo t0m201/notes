1df835b4ee68ee2100e13ba07c8db6e0b9063a03
thomas <tom.finney25@googlemail.com>
2016-10-30 14:50:02 +0000

    notes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,51 @@
+class NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      flash[:success] = "Successfully created..."
+      redirect_to notes_path
+    else
+      flash[:error] = "ERROR"
+      render 'new'
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+   @note = Note.find(params[:id])
+   if @note.update_attributes(note_params)
+     flash[:success] = "Updated!"
+     redirect_to notes_path
+   else
+     render 'edit'
+   end
+ end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy if Note.find(params[:id])
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:id, :title, :body)
+  end
+
+end
